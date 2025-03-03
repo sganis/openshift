69fe78175a76d083ba88a5c7b434872d424d27af
San <sganis@gmail.com>
2025-03-03 18:46:06 +0300

    Added new app

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker!!"
+    host = os.environ.get('HOSTNAME')
+    return f"Flask inside Docker!!. Hostname: {host}"
 
 
 if __name__ == "__main__":
-    
+
     app.run(debug=True,host='0.0.0.0',port=8080)
