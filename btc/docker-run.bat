a1e7f3c5898400b020b9f25d43cac8da4b41c026
San <sganis@gmail.com>
2025-04-12 12:22:31 +0300

    btc node working in prune mode with testnet

--- btc/docker-run.bat
+++ btc/docker-run.bat
@@ -5,6 +5,7 @@ docker run -d ^
   -p 18332:18332 ^
   bitcoin/bitcoin ^
   -testnet=1 ^
+  -assumevalid=000000000f0ff6905c39ad4b7d26b36dd0d38d067002f4ff696b76d4c233c931 ^
   -prune=550 ^
   -txindex=0 ^
   -server=1 ^
