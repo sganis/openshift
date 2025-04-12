41b1cb082b22bbb55adfabd14e7d841c1e3c5008
San <sganis@gmail.com>
2025-04-12 15:58:16 +0300

    Added lnd

--- btc/docker-run.bat
+++ btc/docker-run.bat
@@ -1,16 +1,52 @@
 docker run -d ^
-  --name=bitcoind-testnet ^
-  -v F:\btc-testnet:/home/bitcoin/.bitcoin ^
-  -p 18333:18333 ^
-  -p 18332:18332 ^
+  --name=bitcoind ^
+  -v F:\btc-regtest:/home/bitcoin/.bitcoin ^
+  -p 18444:18444 ^
+  -p 18443:18443 ^
+  -p 28332:28332 ^
+  -p 28333:28333 ^
   bitcoin/bitcoin ^
-  -testnet=1 ^
-  -assumevalid=000000000f0ff6905c39ad4b7d26b36dd0d38d067002f4ff696b76d4c233c931 ^
-  -prune=550 ^
-  -txindex=0 ^
+  -regtest=1 ^
+  -txindex=1 ^
   -server=1 ^
+  -rpcpassword=Secret1! ^
   -rpcbind=0.0.0.0 ^
   -rpcallowip=0.0.0.0/0 ^
   -rpcuser=bitcoin ^
-  -rpcpassword=secret
+  -zmqpubrawblock=tcp://0.0.0.0:28332 ^
+  -zmqpubrawtx=tcp://0.0.0.0:28333
+  
+# test:
+alias bcli='bitcoin-cli -regtest -rpcuser=bitcoin -rpcpassword=Secret1! -rpcconnect=127.0.0.1 -rpcport=18443'
+bcli getblockchaininfo
+bcli createwallet "mywallet"
+bcli -rpcwallet=mywallet getnewaddress
+bcli -rpcwallet=mywallet generatetoaddress 110 bcrt1qjh227d9nkfwzyewgq69n5nmulegs8wduagnhcc
+bcli -rpcwallet=mywallet getbalance
+bcli -rpcwallet=mywallet listunspent
+bcli -rpcwallet=mywallet getblockcount
+bcli -rpcwallet=mywallet listreceivedbyaddress 0 true
+
+docker run -d ^
+  --name lnd ^
+  -p 9735:9735 ^
+  -p 10009:10009 ^
+  -p 8080:8080 ^
+  -v F:\lnd-simnet:/root/.lnd ^
+  lightninglabs/lnd:v0.19.0-beta.rc2 ^
+  lnd ^
+  --bitcoin.regtest ^
+  --bitcoin.node=bitcoind ^
+  --bitcoind.rpchost=host.docker.internal:18443 ^
+  --bitcoind.rpcuser=bitcoin ^
+  --bitcoind.rpcpass=Secret1! ^
+  --bitcoind.zmqpubrawblock=tcp://host.docker.internal:28332 ^
+  --bitcoind.zmqpubrawtx=tcp://host.docker.internal:28333 ^
+  --restlisten=0.0.0.0:8080 ^
+  --rpclisten=0.0.0.0:10009 ^
+  --listen=0.0.0.0:9735 
+
+# test:
+lncli --network=regtest create
+lncli --network=regtest getinfo
 
